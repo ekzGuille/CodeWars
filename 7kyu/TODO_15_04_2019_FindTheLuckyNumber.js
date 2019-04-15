d6f99c0160c6588c0813ccf9932f3ede482e568c
ekzGuille <guillermosesesantos@hotmail.com>
2019-04-15 14:07:37 +0200

    15/04/2019

---
+++ 7kyu/TODO_15_04_2019_FindTheLuckyNumber.js
@@ -0,0 +1,9 @@
+// 15/04/2019
+// https://www.codewars.com/kata/find-the-lucky-numbers
+
+var filterLucky = x => {
+    return 'lucky numbers';
+}
+
+console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]), [7, 70, 17]);
+console.log(filterLucky([71, 9907, 69]), [71, 9907]);
